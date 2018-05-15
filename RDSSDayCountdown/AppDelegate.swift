20f8b5825c7668c4c06c95657cf8bfb8a4dd556e
Marcus Wu <marcus.wu0814@gmail.com>
2018-05-15 19:54:07 +0800

    Initial Commit

---
+++ RDSSDayCountdown/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RDSSDayCountdown
+//
+//  Created by Marcus Wu on 2018/5/15.
+//  Copyright © 2018年 Marcus Wu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RDSSDayCountdown/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RDSSDayCountdown
+//
+//  Created by Marcus Wu on 2018/5/15.
+//  Copyright © 2018年 Marcus Wu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
