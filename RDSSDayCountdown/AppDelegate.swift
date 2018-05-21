542c1d47b97663f7c1fca10f2033fe19cc5f950c
finnho <finnho@truetel.com>
2018-05-21 20:03:32 +0800

    Add item in navigation bar

--- RDSSDayCountdown/AppDelegate.swift
+++ RDSSDayCountdown/AppDelegate.swift
@@ -11,16 +11,23 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        if let button = statusItem.button {
+            button.title = "Test"
+            button.action = #selector(printQuote(_:))
+        }
+        
     }
-
+    
+    @objc func printQuote(_ sender: Any?) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
+        
+        print("\(quoteText) — \(quoteAuthor)")
+    }
 
 }
 
